b5e60e99ea613189b7f9c558ab6f3ace3b4de5b6
mohamed Samray <m.m.samray@users.noreply.github.com>
2026-06-13 08:44:40 +0300

    Multi Providers Fixed

--- Pure Pets/MainApp/UserFiles/UsesHelpers/UserManager.m
+++ Pure Pets/MainApp/UserFiles/UsesHelpers/UserManager.m
@@ -2901,18 +2901,43 @@ static NSMutableDictionary<NSString*, UserModel*> *userCacheByUID;
         return;
     }
 
-    [[[[FIRFirestore firestore] collectionWithPath:@"PublicUserProfiles"] documentWithPath:uid]
-     getDocumentWithCompletion:^(FIRDocumentSnapshot * _Nullable snapshot, NSError * _Nullable error) {
+    FIRFirestore *db = [FIRFirestore firestore];
+    FIRDocumentReference *publicProfileRef = [[db collectionWithPath:@"PublicUserProfiles"] documentWithPath:uid];
+    FIRDocumentReference *userRef = [[db collectionWithPath:@"UsersCol"] documentWithPath:uid];
+
+    void (^completeFromSnapshot)(FIRDocumentSnapshot * _Nullable, NSError * _Nullable) =
+    ^(FIRDocumentSnapshot * _Nullable snapshot, NSError * _Nullable error) {
         if (error) {
-            completion(nil, error);
+            if (completion) completion(nil, error);
             return;
         }
-        if (snapshot.exists) {
-            UserModel *model = [[UserModel alloc] initWithSnapshot:snapshot];
-            completion(model, nil);
-        } else {
-            completion(nil, [NSError errorWithDomain:@"UserManager" code:404 userInfo:@{NSLocalizedDescriptionKey: @"User not found"}]);
+        if (!snapshot.exists) {
+            if (completion) {
+                completion(nil, [NSError errorWithDomain:@"UserManager"
+                                                    code:404
+                                                userInfo:@{NSLocalizedDescriptionKey: @"User not found"}]);
+            }
+            return;
         }
+
+        UserModel *model = [[UserModel alloc] initWithSnapshot:snapshot];
+        if (completion) completion(model, nil);
+    };
+
+    [publicProfileRef getDocumentWithCompletion:^(FIRDocumentSnapshot * _Nullable snapshot, NSError * _Nullable error) {
+        if (error) {
+            if (completion) completion(nil, error);
+            return;
+        }
+
+        if (snapshot.exists) {
+            completeFromSnapshot(snapshot, nil);
+            return;
+        }
+
+        [userRef getDocumentWithCompletion:^(FIRDocumentSnapshot * _Nullable fallbackSnapshot, NSError * _Nullable fallbackError) {
+            completeFromSnapshot(fallbackSnapshot, fallbackError);
+        }];
     }];
 }
 
